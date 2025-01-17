5278c551c11e0d10f88eeec5697fb943a7c20d5d
kimwlsgh33 <kimwlsgh33@gmail.com>
2025-01-18 00:59:00 +0900

    fix(name): fix the directory name to specify

--- lua/learn-lang/init.lua
+++ lua/learn-lang/init.lua
@@ -1,3 +1,6 @@
+-- This is where your plugin's code lives.
+-- The code here will only be executed when the user explicitly requires your plugin.
+
 local function create_buffer()
   local buf = vim.api.nvim_create_buf(true, true)
   vim.api.nvim_buf_set_name(buf, "*scratch*")
--- plugin/example.lua
+++ plugin/example.lua
@@ -1,1 +1,3 @@
+-- These are executed when Neovim starts.
+-- This is useful for setting up keymaps or autocommands that should be available regardless of whether the user requires the plugin or not.
 print("plugin/example.lua is executed!")
